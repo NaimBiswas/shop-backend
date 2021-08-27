cebc7f8f3ef4e296e12d5d58ae9be160a23e663c
NaimBiswas <nayeembiswas2@gmail.com>
2021-08-27 22:31:15 +0600

    add validation

--- Routes/ProductRouter.js
+++ Routes/ProductRouter.js
@@ -5,26 +5,33 @@ Router.post('/add-product', function (req, res) {
    const userId = req.headers['id']
    const { title, imageURL, price, description } = req.body
    if (userId) {
-      const newProduct = new productModel({
-         userId: userId,
-         title: title,
-         imageURL: imageURL,
-         price: price,
-         description: description,
-      })
-      newProduct.save().then((result) => {
-         res.status(201).json({
-            message: "Thanks for create a new Product",
-            succ: true,
-            data: result
+      if (title && imageURL && price && description) {
+         const newProduct = new productModel({
+            userId: userId,
+            title: title,
+            imageURL: imageURL,
+            price: price,
+            description: description,
          })
-      }).catch((err) => {
+         newProduct.save().then((result) => {
+            res.status(201).json({
+               message: "Thanks for create a new Product",
+               succ: true,
+               data: result
+            })
+         }).catch((err) => {
+            res.status(400).json({
+               message: "Something happened wrong!",
+               succ: false,
+               error: err,
+            })
+         })
+      } else {
          res.status(400).json({
-            message: "Something happened wrong!",
+            message: "Fill all those field and try again!",
             succ: false,
-            error: err,
          })
-      })
+      }
    } else {
       res.status(400).json({
          message: "Send user id by headers",
